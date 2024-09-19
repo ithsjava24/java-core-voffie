cb834f26e9c4163f558bcff73ffb69c93502c1f7
voffie <viktorsoderborg@gmail.com>
2024-09-19 16:41:18 +0200

    Implements getInstance and constructor for Warehouse class

---
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -0,0 +1,32 @@
+package org.example.warehouse;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Warehouse {
+    private final String name;
+    private final static Map<String, Warehouse> instances = new HashMap<>();
+
+    private Warehouse() {
+        this.name = "Default";
+    }
+
+    private Warehouse(String name) {
+        this.name = name;
+    }
+
+    public static Warehouse getInstance() {
+        return new Warehouse();
+    }
+
+    public static Warehouse getInstance(String name) {
+        if (instances.containsKey(name)) {
+            return instances.get(name);
+        } else {
+            Warehouse warehouse = new Warehouse(name);
+            instances.put(name, warehouse);
+            return warehouse;
+        }
+    }
+}
+
